733d30b422ef7b465fb792c010a5128849369e2c
AndLOLGG <andreassilas@gmail.com>
2025-02-26 10:40:06 +0100

    Initial commit

---
+++ src/main/java/org/example/sprbmvn/SprBmvnApplication.java
@@ -0,0 +1,14 @@
+package org.example.sprbmvn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprBmvnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprBmvnApplication.class, args);
+        System.out.println("Hello!");
+    }
+
+}
